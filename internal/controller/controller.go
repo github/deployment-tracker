9800cfa51856db32fdd26270547af74832c27bda
Eric Pickard <piceri@github.com>
2026-02-03 11:09:51 -0500

    add exclude namespaces flag

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -52,20 +52,9 @@ type Controller struct {
 }
 
 // New creates a new deployment tracker controller.
-func New(clientset kubernetes.Interface, namespace string, cfg *Config) (*Controller, error) {
+func New(clientset kubernetes.Interface, namespace string, excludeNamespaces string, cfg *Config) (*Controller, error) {
 	// Create informer factory
-	var factory informers.SharedInformerFactory
-	if namespace == "" {
-		factory = informers.NewSharedInformerFactory(clientset,
-			30*time.Second,
-		)
-	} else {
-		factory = informers.NewSharedInformerFactoryWithOptions(
-			clientset,
-			30*time.Second,
-			informers.WithNamespace(namespace),
-		)
-	}
+	factory := createInformerFactory(clientset, namespace, excludeNamespaces)
 
 	podInformer := factory.Core().V1().Pods().Informer()
 
@@ -486,6 +475,40 @@ func (c *Controller) recordContainer(ctx context.Context, pod *corev1.Pod, conta
 
 func getCacheKey(dn, digest string) string {
 	return dn + "||" + digest
+}
+
+// createInformerFactory creates a shared informer factory with the given resync period.
+// If excludeNamespaces is non-empty, it will exclude those namespaces from being watched.
+// If namespace is non-empty, it will only watch that namespace.
+func createInformerFactory(clientset kubernetes.Interface, namespace string, excludeNamespaces string) informers.SharedInformerFactory {
+	var factory informers.SharedInformerFactory
+	if namespace != "" {
+		factory = informers.NewSharedInformerFactoryWithOptions(
+			clientset,
+			30*time.Second,
+			informers.WithNamespace(namespace),
+		)
+	} else if excludeNamespaces != "" {
+		excludedNamespacesList := strings.Split(excludeNamespaces, ",")
+		for i := 0; i < len(excludedNamespacesList); i++ {
+			excludedNamespacesList[i] = fmt.Sprintf("metadata.namespace!=%s", strings.TrimSpace(excludedNamespacesList[i]))
+		}
+		tweakListOptions := func(options *metav1.ListOptions) {
+			options.FieldSelector = strings.Join(excludedNamespacesList, ",")
+		}
+
+		factory = informers.NewSharedInformerFactoryWithOptions(
+			clientset,
+			30*time.Second,
+			informers.WithTweakListOptions(tweakListOptions),
+		)
+	} else {
+		factory = informers.NewSharedInformerFactory(clientset,
+			30*time.Second,
+		)
+	}
+
+	return factory
 }
 
 // getARDeploymentName converts the pod's metadata into the correct format
